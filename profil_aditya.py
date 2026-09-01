7f2571897a027661edb7be9a2c6b81ca92cd3e17
ADITYA PRASETYO NUGROHO <prasetyonugroho683@gmail.com>
2026-09-01 15:32:57 +0700

    Add files via upload

---
+++ profil_aditya.py
@@ -0,0 +1,28 @@
+Nama=input("Masukan Nama:")
+Kelas=input("Masukan Kelas:")
+Umur=int(input("Masukan Umur:"))
+Jurusan=input("Masukan Jurusan:")
+Hobby=input("Masukan Hobby Anda:")
+print  (
+
+
+
+
+)
+print("=====MY PROFILE=====")
+print(
+
+
+)
+print("Nama Saya    :", Nama)
+print("Saya Kelas   :", Kelas)
+print("Saya Umur    :", Umur, "Tahun")
+print("Jurusan Saya :", Jurusan)
+print("Hobby Saya   :", Hobby)
+
+
+
+
+
+
+
