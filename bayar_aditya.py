4b4d96f1bc32f15f31b523bd1efb27b546d337eb
ADITYA PRASETYO NUGROHO <prasetyonugroho683@gmail.com>
2026-09-08 14:02:42 +0700

    Add files via upload

---
+++ bayar_aditya.py
@@ -0,0 +1,6 @@
+harga = int(input("Harga barang: "))
+jumlah = int(input("Jumlah barang: "))
+
+total = harga * jumlah
+
+print("Total pembayaran:", total)
---
+++ calc_sederhana_aditya.py
@@ -0,0 +1,9 @@
+angka1 = 10
+angka2 = 3
+
+print("Penjumlahan:", angka1 + angka2)
+print("Pengurangan:", angka1 - angka2)
+print("Perkalian:", angka1 * angka2)
+print("pembagian:", angka1 / angka2)
+print("Sisa bagi:", angka1 % angka2)
+print("Pangkat:", angka1 ** angka2)
